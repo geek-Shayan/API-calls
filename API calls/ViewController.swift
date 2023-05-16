82f8713b2a7483621be86aec14de5f727af927a9
geek-Shayan <shayanulhaqsadi@gmail.com>
2023-05-16 14:53:18 +0600

    Initial Commit

---
+++ API calls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  API calls
+//
+//  Created by MD. SHAYANUL HAQ SADI on 16/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
